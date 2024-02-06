e565674b7085f01f9664c943cee58c4f9a6dc5b9
Jaiharan <136414923+Jaiharan@users.noreply.github.com>
2024-02-06 23:14:38 +0530

    Create Repo

---
+++ src/app/page.js
@@ -0,0 +1,12 @@
+/* useClient */
+import Main from './components/Main';
+import Navbar from './components/Navbar';
+
+export default function Home() {
+  return (
+    <main>
+      <Navbar />
+      <Main />
+    </main>
+  );
+}
